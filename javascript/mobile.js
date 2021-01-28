9d1d3159efa3c17a783ed9fa6ede82d7cb4e463a
Alizeh Khan <alizehedinburgh@gmail.com>
2021-01-28 20:18:54 +0100

    done

--- javascript/mobile.js
+++
@@ -1,9 +0,0 @@
-if ([
-    /Android/i,
-    /iPhone/i,
-    /iPad/i,
-    /iPod/i,
-    /BlackBerry/i,
-    /Windows Phone/i
-  ].some(toMatchItem => {return navigator.userAgent.match(toMatchItem)}))
-  document.body.innerHTML = `<img id="errorImage" src="resources/images/phone-man.png">`
